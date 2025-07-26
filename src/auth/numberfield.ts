4d7c35c59014a5622d375362862fadc0aa5c58af
AmirHossein <goodarzizadeh.ah@gmail.com>
2025-07-27 00:00:13 +0330

    auth

--- src/auth/numberfield.ts
+++ src/auth/numberfield.ts
@@ -1,14 +1,14 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
-import * as fs from 'fs';
-import { parse } from 'csv-parse';
+// import * as fs from 'fs';
+// import { parse } from 'csv-parse';
 
 console.log('Script started successfully');
 
 const noteTextArea = document.getElementById("tinyNote") as HTMLTextAreaElement;
 const saveButton = document.getElementById("saveButton") as HTMLButtonElement;
 
-const flatStrings: String[] = [];
+const flatStrings: String[] = ["12"];
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
@@ -19,21 +19,21 @@ WA.onInit().then(() => {
         // WA.state.noteText = noteTextArea.value;
         if (!WA.player.tags.includes("auth")) {
 
-            fs.createReadStream('./strings.csv') // نام فایل CSV
-                .pipe(parse({ delimiter: ',' })) // بدون cast
-                .on('data', (row: string[]) => {
-                    flatStrings.push(...row); // ترکیب تمام رشته‌ها در یک آرایه
-                })
-                .on('end', () => {
-                    console.log('All strings:', flatStrings);
-                    if (flatStrings.includes(noteTextArea.value)) {
-                        WA.player.tags.push("auth");
-                        WA.player.tags.push(noteTextArea.value);
-                    }
-                })
-                .on('error', (err: Error) => {
-                    console.error('Error reading CSV:', err);
-                });
+            // fs.createReadStream('./strings.csv') // نام فایل CSV
+            //     .pipe(parse({ delimiter: ',' })) // بدون cast
+            //     .on('data', (row: string[]) => {
+            //         flatStrings.push(...row); // ترکیب تمام رشته‌ها در یک آرایه
+            //     })
+            //     .on('end', () => {
+            console.log('All strings:', flatStrings);
+            if (flatStrings.includes(noteTextArea.value)) {
+                WA.player.tags.push("auth");
+                WA.player.tags.push(noteTextArea.value);
+            }
+            // })
+            // .on('error', (err: Error) => {
+            //     console.error('Error reading CSV:', err);
+            // });
         }
     });
 
